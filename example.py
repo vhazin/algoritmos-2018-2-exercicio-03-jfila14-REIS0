3cc9d884dd91df68729b1a1881499e070acabad9
Arthur O. Reis <aor@cesar.school>
2018-08-28 13:58:54 -0300

    example

--- example.py
+++ example.py
@@ -1,14 +1,16 @@
 # tamanho e identificadores da fila
 N = int(input(""))
-lista = [str(input("")).split() for i in range(N)]
+lista = str(input("")).split()
 
 
 # items a se remover
 M = int(input(""))
 if M > N or N > 50000 or M > 50000:
     exit(1)
-remove = [str(input("")).split() for j in range(M)]
+remove = str(input("")).split()
 
+if len(lista) > N or len(remove) > M:
+    exit(1)
 #checa os itens para remover da lista principal
 for i in lista:
     for j in remove:
