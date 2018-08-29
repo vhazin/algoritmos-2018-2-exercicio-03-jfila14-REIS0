fd2c494612fd0f38fd8eda61b83d292b7d7523bf
Arthur O. Reis <aor@cesar.school>
2018-08-28 22:32:20 -0300

    example backup

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 # tamanho e identificadores da fila
 N = int(input(""))
-lista = str(input("")).split()
-
+fila = str(input("")).split()
 
 # items a se remover
 M = int(input(""))
@@ -9,13 +8,16 @@ if M > N or N > 50000 or M > 50000:
     exit(1)
 remove = str(input("")).split()
 
-if len(lista) > N or len(remove) > M:
+if len(fila) > N or len(remove) > M:
     exit(1)
 #checa os itens para remover da lista principal
-for i in lista:
-    for j in remove:
-        if lista[i] == remove[j]:
-            lista.remove(lista[i])
-            print("infinito")
+j = 0
+i = 0
+print(fila[i])
+while j < M:
+    if fila[i] == remove[j]:
+        fila.remove(fila[i])
+        j += 1
+    i += 1
 
-print(" ".join(lista))
+print(" ".join(fila))
