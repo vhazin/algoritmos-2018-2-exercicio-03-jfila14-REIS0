666bd0701b68f1369c3cf975e6069b48e27c769e
Arthur O. Reis <aor@cesar.school>
2018-08-30 20:35:01 -0300

    backup

--- lista.c
+++ lista.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <malloc.h>
 
-struct node {
+typedef struct Node {
   int val;
   struct node * next;
 } node ;
 
-struct list {
+typedef struct List {
   int size;
   struct node * data;
 } list ;
@@ -14,14 +14,112 @@ struct list {
 
 list * createlist() {
   list * head;
-  head = malloc(list);
+  head = malloc(sizeof(list));
 
   if (head != NULL) {
-    head.list = 0;
-    head.data = NULL;
+    head->size = 0;
+    head->data = NULL;
   }
-  return list;
+  return (head);
 }
+
+int addlist(list * head, int * val, int position) {
+
+  int i;
+  node * new;
+  node * before;
+
+  if (position > head->size + 1) {
+    return 0;
+  }
+  else {
+
+    new = malloc(sizeof(node));
+
+    if (position == 1) {
+      head->size = head->size + 1;
+      new->val = val;
+      new->next = head->data;
+      head->data = new;
+      return (head->size);
+    }
+
+    else {
+
+      if (new == NULL) {
+        return 0;
+      }
+      else{
+        before = head->data;
+        for (i = 0; i < (position - 2); i++)
+          before = before->next;
+
+        new->next = before->next;
+        new->val = val;
+        before->next = new;
+        head->size = head->size + 1;
+        return (head->size);
+      }
+    }
+  }
+}
+
+
+int retirar(list * head, int value) {
+
+  node * before;
+  node * aux;
+  int * back;
+  aux = head->data;
+
+  while (aux != NULL) {
+    if (aux->val == value) {
+      if (before == NULL) {
+        head->data = aux->next;
+      }
+      else{
+        before->next = (head->size - 1);
+      }
+      head->size = (head->size - 1);
+      return 1;
+    }
+
+  before = aux;
+  aux = aux->next;
+  }
+}
+
+
+void display(node * head) {
+  while (head != NULL) {
+    printf("%d ", head->val);
+    head = head->next;
+  }
+  printf("\n");
+}
+
+
+void destroy(node * head) {
+  node * temp;
+  node * before;
+
+  if (empty(head)) {
+    free(head);
+  }
+  else {
+    temp = head->data;
+
+    while (temp != NULL) {
+      before = temp;
+      temp = temp->next;
+      free(before->val);
+      free(before);
+    }
+    free(head);
+  }
+
+}
+
 
 
 int main() {
@@ -31,12 +129,12 @@ int main() {
   scanf("%d", N);
   int fila[N];
   while (i < N && scanf("%d", &fila[i]) == 1){
-    addlist(list, fila[i], i);
+    addlist(list * head, fila[i], i);
     i++;
   }
   scanf("%d", M);
   int remover[M];
-  while (j < N && scanf("%d", &remover[j]) == 1){
+  while (j < N && scanf("%d", &remover[j]) == 1)
     j++;
 
   for (size_t i = 0; i < N; i++) {
@@ -51,91 +149,4 @@ int main() {
   destroy(node * head);
 
   return 0;
-}
-
-
-
-int addlist(list * head, int * info, int position) {
-
-  int i;
-  node * new;
-  node * before;
-
-  if (position > head.size + 1) {
-    return 0;
-  }
-  else {
-
-    new = malloc(node);
-
-    if (position == 1) {
-      head.size = head.size + 1;
-      new.info = info;
-      new.next = head.data;
-      head.data = new;
-      return (head.size);
-    }
-
-    else {
-
-      if (new == NULL) {
-        return 0;
-      }
-      else{
-        before = head.data;
-        for (i = 0; i < (position - 2); i++)
-          before = before.next;
-
-        new.next = before.next;
-        new.info = info;
-        before.next = new;
-        head.size = head.size + 1;
-        return (head.size);
-      }
-    }
-  }
-}
-
-
-int retirar(list * head, int value) {
-
-  node * before;
-  node * aux;
-  int * back;
-  aux = head.data;
-
-  while (aux != NULL) {
-    if (aux.info == value) {
-      if (before == NULL) {
-        head.data = aux.next;
-      }
-      else{
-        before.next = (head.size - 1);
-      }
-      head.size = (head.size - 1);
-      return 1;
-    }
-
-  before = aux;
-  aux = aux.next;
-  }
-}
-
-
-void display(node * head) {
-  while (head != NULL) {
-    printf("%d ", head->data);
-    head = head->next;
-  }
-  printf("\n");
-}
-
-
-void destroy(node * head) {
-  node * temp;
-  while (*head != NULL) {
-    temp = *head;
-    *head = (*head)->next;
-    free(temp);
-  }
 }
